cea2abbae8ab9284ac1d1c04f316cec692443610
mkpi-pm <mkpi@protonmail.comm>
2022-05-26 15:18:32 +0100

    adding styling to the app

--- src/App.js
+++ src/App.js
@@ -1,40 +1,43 @@
-import './App.css';
-import React from "react"
+import "./App.css";
+import React from "react";
+
 function App() {
-  
-  const [newItem, setNewItem] = React.useState([{updateItem:"update stuff"}]);
+  const [newItem, setNewItem] = React.useState([
+    { updateItem: "update stuff" },
+  ]);
 
+  // function makes buttons that set inputItem
+  //functuon that pushes inputItem into tasks pushes the thing
+  // makes html list ToDolist
+  // addToList => div on line 33
 
-
-
-// function makes buttons that set inputItem
-//functuon that pushes inputItem into tasks pushes the thing
-// makes html list ToDolist
-// addToList => div on line 33
-
-
-
-  return <div className="App">{ToDoList(Tasks)}{button()}</div>;
+  return (
+    <div className="App">
+      <header>
+        <h1 class="App-header">Happy To do list</h1>
+      </header>
+      <div class="Tasks">{ToDoList(Tasks)}</div>
+      {button()}
+    </div>
+  );
 }
 
-export default App;
-  const Tasks = ["Setup react Git", "Delete Stuff"];
+const Tasks = ["Setup react Git", "Delete Stuff"];
 
-  const AddToList = ([Tasks, newItem]) => {
-    let newToDo = [...Tasks];
-    newToDo.push(newItem);
-    console.log(newToDo)
-    return newToDo;
-  };
+const AddToList = ([Tasks, newItem]) => {
+  let newToDo = [...Tasks];
+  newToDo.push(newItem);
+  console.log(newToDo);
+  return newToDo;
+};
 
 const ToDoList = function (newToDo) {
- 
   const toDoArr = newToDo.map((task) => {
     return (
       <>
-        <li className="ToDo-List" key={task}>
-          <li>{task}</li>
-        </li>
+        <p className="ToDo-List" key={task}>
+          {task}
+        </p>
       </>
     );
   });
@@ -45,10 +48,12 @@ const ToDoList = function (newToDo) {
   );
 };
 
-
-
 function button() {
-  return <button onClick={AddToList(Tasks,"Wash Car")}>Wash Car</button> 
+  return (
+    <button class="Button" onClick={AddToList(Tasks, "Add to list")}>
+      Add to list
+    </button>
+  );
 }
 
-
+export default App;
