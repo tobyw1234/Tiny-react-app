bd3e8e37e88ec8d322dd6c9992aaf3d58057b7d8
tobyw1234 <toby0977@gmail.com>
2022-05-26 14:04:00 +0100

    initial layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,54 @@
 import './App.css';
-
+import React from "react"
 function App() {
-  return (
-    <div className="App">
-          </div>
-  );
+  
+  const [newItem, setNewItem] = React.useState([{updateItem:"update stuff"}]);
+
+
+
+
+// function makes buttons that set inputItem
+//functuon that pushes inputItem into tasks pushes the thing
+// makes html list ToDolist
+// addToList => div on line 33
+
+
+
+  return <div className="App">{ToDoList(Tasks)}{button()}</div>;
 }
 
 export default App;
+  const Tasks = ["Setup react Git", "Delete Stuff"];
+
+  const AddToList = ([Tasks, newItem]) => {
+    let newToDo = [...Tasks];
+    newToDo.push(newItem);
+    console.log(newToDo)
+    return newToDo;
+  };
+
+const ToDoList = function (newToDo) {
+ 
+  const toDoArr = newToDo.map((task) => {
+    return (
+      <>
+        <li className="ToDo-List" key={task}>
+          <li>{task}</li>
+        </li>
+      </>
+    );
+  });
+  return (
+    <>
+      <ul className="ToDo-List"> {toDoArr} </ul>
+    </>
+  );
+};
+
+
+
+function button() {
+  return <button onClick={AddToList(Tasks,"Wash Car")}>Wash Car</button> 
+}
+
+
