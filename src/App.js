76054231da034f8b821730a4316f28c3835cbd39
tobyw1234 <toby0977@gmail.com>
2022-05-27 10:33:15 +0100

    laid out intial webpage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import "./App.css";
 import React from "react";
-
+import {useState} from "react"
 function App() {
-  const [newItem, setNewItem] = React.useState([
-    { updateItem: "update stuff" },
-  ]);
+//   const [newItem, setNewItem] = React.useState([
+//     { updateItem: "update stuff" },
+//   ]);
 
-  // function makes buttons that set inputItem
+//   const Tasks = ["Setup react Git", "Delete Stuff"];
+//   let workingTasks = [...Tasks];   
+//   console.log(workingTasks);
+
+//   function randomise(workingTasks) {
+//     let randomInt = Math.floor(Math.random() * 2);
+//     // WorkingTasks.push(WorkingTasks.slice(0, randomInt))
+//     console.log(Array.isArray(workingTasks));
+//   }
+
+  // function button() {
+  //   return (
+  //     <button className="Button" onClick={randomise}>
+  //       Add to list
+  //     </button>
+  //   );
+  // }
+  // // function makes buttons that set inputItem
   //functuon that pushes inputItem into tasks pushes the thing
   // makes html list ToDolist
   // addToList => div on line 33
@@ -14,46 +31,36 @@ function App() {
   return (
     <div className="App">
       <header>
-        <h1 class="App-header">Happy To do list</h1>
+        <h1 className="App-header">Happy To do list</h1>
       </header>
-      <div class="Tasks">{ToDoList(Tasks)}</div>
-      {button()}
+      <ToDoList />
     </div>
   );
 }
 
-const Tasks = ["Setup react Git", "Delete Stuff"];
-
-const AddToList = ([Tasks, newItem]) => {
-  let newToDo = [...Tasks];
-  newToDo.push(newItem);
-  console.log(newToDo);
-  return newToDo;
-};
-
-const ToDoList = function (newToDo) {
-  const toDoArr = newToDo.map((task) => {
-    return (
-      <>
-        <p className="ToDo-List" key={task}>
-          {task}
-        </p>
-      </>
-    );
-  });
+const ToDoList = function () {
+  const firstToDo = ["Setup react Git", "Delete Stuff"];
+  const [toDo, setToDo] = useState(firstToDo);
   return (
-    <>
-      <ul className="ToDo-List"> {toDoArr} </ul>
-    </>
+    <div className="ToDo-List">
+      <h2>Things to do</h2>
+      <ul>
+        {toDo.map((item) => {
+          return <li key={item}>{item}</li>;
+        })}
+      </ul>
+      <AddToList toDo={toDo} />
+    </div>
   );
 };
 
-function button() {
-  return (
-    <button class="Button" onClick={AddToList(Tasks, "Add to list")}>
-      Add to list
-    </button>
-  );
-}
+const AddToList = ({ setToDo }) => {
+  console.log(setToDo);
+  return <button className="Button"> button</button>;
+};
 
 export default App;
+// Ideas
+// 
+/// fuction that deletes an item from the list
+/// function where you click a button and it randomizes the list 
